820fc5b755ab85f012f7032323058c3928a5fc7f
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2020-12-03 21:38:15 +0100

    Setup js env

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of code 2020
+
+Check [adventofcode website](https://adventofcode.com/2020) for more info.
+
+These implementations are just for fun and not meant to be performant / elegant / best practices...
---
+++ template.js
@@ -0,0 +1,10 @@
+const input = [
+  // TODO
+]
+
+// TODO
+const solvePart1 = () => {}
+const solvePart2 = () => {}
+
+console.log('solution for part 1:', solvePart1(input))
+console.log('solution for part 2:', solvePart2(input))
